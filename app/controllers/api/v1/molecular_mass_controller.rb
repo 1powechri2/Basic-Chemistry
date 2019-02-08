034652b114f3437b8201cafff51bcadd37dda348
Chris Powell <chris.ian.powell@gmail.com>
2019-02-08 10:48:35 -0700

    This is a whatever debugging for the angular app

--- app/controllers/api/v1/molecular_mass_controller.rb
+++ app/controllers/api/v1/molecular_mass_controller.rb
@@ -4,7 +4,6 @@ class Api::V1::MolecularMassController < ApplicationController
     calculator = MolecularMassCalculator.new
     count = counter.element_counter(params[:molecule])
     seperated = calculator.seperate_elements_from_count(count)
-    binding.pry
     render json: {molecule: params[:molecule],
                   molecular_mass: calculator.calculate_molecular_mass(seperated)}
   end
